af6234345854a3d876b6c6711228b08d4e0acf73
githubjoahan <joahan.alvaro8@gmail.com>
2023-10-27 00:51:57 -0600

    Se modificaron algos textos

--- src/components/Home.jsx
+++ src/components/Home.jsx
@@ -10,7 +10,7 @@ const Home = ({ correoUsuario }) => {
   // eslint-disable-next-line no-unused-vars
   const [ejercicios, setEjercicios] = useState([
     {
-      nombre: "Flexiones del couch leandro montejo",
+      nombre: "Flexiones ",
       pasos: [
         "Colócate boca abajo, con las manos a la altura de los hombros.",
         "Eleva tu cuerpo usando la fuerza de tus brazos.",
@@ -18,7 +18,7 @@ const Home = ({ correoUsuario }) => {
       ],
     },
     {
-      nombre: "Sentadillas",
+      nombre: "Sentadillas joahan",
       pasos: [
         "De pie, con las piernas separadas al ancho de los hombros.",
         "Baja tus caderas como si fueras a sentarte en una silla.",
