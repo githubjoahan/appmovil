b88f5342c26639ab2728c10d65407ec7b985b0b0
githubjoahan <joahan.alvaro8@gmail.com>
2023-12-07 00:56:44 -0600

    Resultados de liberación

---
+++ Object Repository/Page_Vite  React/div_Inicia sesionNo tienes cuentaRegistrate.rs
@@ -0,0 +1,67 @@
+<?xml version="1.0" encoding="UTF-8"?>
+<WebElementEntity>
+   <description></description>
+   <name>div_Inicia sesionNo tienes cuentaRegistrate</name>
+   <tag></tag>
+   <elementGuidId>40a08242-c19d-4c3e-812f-b2bdd3856166</elementGuidId>
+   <selectorCollection>
+      <entry>
+         <key>CSS</key>
+         <value>#root > div</value>
+      </entry>
+      <entry>
+         <key>XPATH</key>
+         <value>//div[@id='root']/div</value>
+      </entry>
+   </selectorCollection>
+   <selectorMethod>XPATH</selectorMethod>
+   <useRalativeImagePath>true</useRalativeImagePath>
+   <webElementProperties>
+      <isSelected>false</isSelected>
+      <matchCondition>equals</matchCondition>
+      <name>tag</name>
+      <type>Main</type>
+      <value>div</value>
+      <webElementGuid>f0095945-9ea7-4cbe-a7ca-a1b66ebadde7</webElementGuid>
+   </webElementProperties>
+   <webElementProperties>
+      <isSelected>true</isSelected>
+      <matchCondition>equals</matchCondition>
+      <name>text</name>
+      <type>Main</type>
+      <value>Inicia sesionNo tienes cuentaRegistrate</value>
+      <webElementGuid>02ddefc2-3227-4b1d-84d6-5a00c2218ef3</webElementGuid>
+   </webElementProperties>
+   <webElementProperties>
+      <isSelected>false</isSelected>
+      <matchCondition>equals</matchCondition>
+      <name>xpath</name>
+      <type>Main</type>
+      <value>id(&quot;root&quot;)/div[1]</value>
+      <webElementGuid>15ad91ea-2d6d-4043-9a3b-4c7c411e195b</webElementGuid>
+   </webElementProperties>
+   <webElementXpaths>
+      <isSelected>true</isSelected>
+      <matchCondition>equals</matchCondition>
+      <name>xpath:idRelative</name>
+      <type>Main</type>
+      <value>//div[@id='root']/div</value>
+      <webElementGuid>9372f9bb-572e-486f-8b81-bfe869519b92</webElementGuid>
+   </webElementXpaths>
+   <webElementXpaths>
+      <isSelected>false</isSelected>
+      <matchCondition>equals</matchCondition>
+      <name>xpath:position</name>
+      <type>Main</type>
+      <value>//div/div</value>
+      <webElementGuid>f5ab7aff-a28e-4e0a-ba53-6c49abbf04f0</webElementGuid>
+   </webElementXpaths>
+   <webElementXpaths>
+      <isSelected>false</isSelected>
+      <matchCondition>equals</matchCondition>
+      <name>xpath:customAttributes</name>
+      <type>Main</type>
+      <value>//div[(text() = 'Inicia sesionNo tienes cuentaRegistrate' or . = 'Inicia sesionNo tienes cuentaRegistrate')]</value>
+      <webElementGuid>11befc7f-8d72-4ba3-9a80-ae21258f2466</webElementGuid>
+   </webElementXpaths>
+</WebElementEntity>
